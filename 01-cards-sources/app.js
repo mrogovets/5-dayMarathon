5343c9e4c023000a4af1d99ff27f3e30f57535f7
mrogovets <mrogovets@ukr.net>
2021-10-01 18:05:22 +0300

    upgrade day-1 (created slidesPlugin)

--- 01-cards-sources/app.js
+++ 01-cards-sources/app.js
@@ -1,14 +1,20 @@
-const slides = document.querySelectorAll(".slide");
+const slidesPlugin = (activeSlide = 0) => {
+  const slides = document.querySelectorAll(".slide");
 
-for (const slide of slides) {
-  slide.addEventListener("click", () => {
-    clearActiveClasses();
-    slide.classList.add("active");
-  });
-}
+  slides[activeSlide].classList.add("active");
 
-const clearActiveClasses = () => {
-  slides.forEach((slide) => {
-    slide.classList.remove("active");
-  });
+  for (const slide of slides) {
+    slide.addEventListener("click", () => {
+      clearActiveClasses();
+      slide.classList.add("active");
+    });
+  }
+
+  const clearActiveClasses = () => {
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+  };
 };
+
+slidesPlugin();
