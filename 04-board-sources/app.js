780a8ac673a7a7ddbc17454afc0f4762ce7b76cf
mrogovets <mrogovets@ukr.net>
2021-10-02 00:49:18 +0300

    homeWork day-4 done

--- 04-board-sources/app.js
+++ 04-board-sources/app.js
@@ -1,1 +1,43 @@
+const board = document.querySelector("#board");
+const colors = [
+  "tomato",
+  "GreenYellow",
+  "HotPink",
+  "Ivory",
+  "Magenta",
+  "MediumBlue",
+  "MediumSpringGreen",
+  "Orange",
+];
+const SQUARES_NUMBER = 600;
 
+const setColor = (element) => {
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+};
+
+const removeColor = (element) => {
+  element.style.backgroundColor = "#1d1d1d";
+  element.style.boxShadow = `0 0 2px #000`;
+};
+
+const getRandomColor = () => {
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
+};
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+
+  square.addEventListener("mouseover", () => {
+    setColor(square);
+  });
+
+  square.addEventListener("mouseleave", () => {
+    removeColor(square);
+  });
+
+  board.append(square);
+}
