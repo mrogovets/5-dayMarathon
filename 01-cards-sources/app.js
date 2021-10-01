6d80aa9a9863aba6ee796ac972c3562faeff86df
mrogovets <mrogovets@ukr.net>
2021-10-01 17:47:56 +0300

    first commit

---
+++ 01-cards-sources/app.js
@@ -0,0 +1,14 @@
+const slides = document.querySelectorAll(".slide");
+
+for (const slide of slides) {
+  slide.addEventListener("click", () => {
+    clearActiveClasses();
+    slide.classList.add("active");
+  });
+}
+
+const clearActiveClasses = () => {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+};
