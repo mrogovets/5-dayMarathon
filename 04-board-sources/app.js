f29390a4a33d3cb55a255fab5a8abc6e22d532ca
mrogovets <mrogovets@ukr.net>
2021-10-02 10:37:33 +0300

    apdate setColor(event) & removeColor(event)

--- 04-board-sources/app.js
+++ 04-board-sources/app.js
@@ -11,33 +11,30 @@ const colors = [
 ];
 const SQUARES_NUMBER = 600;
 
-const setColor = (element) => {
+const setColor = (event) => {
+  const element = event.target;
   const color = getRandomColor();
   element.style.backgroundColor = color;
   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 };
 
-const removeColor = (element) => {
+const removeColor = (event) => {
+  const element = event.target;
   element.style.backgroundColor = "#1d1d1d";
   element.style.boxShadow = `0 0 2px #000`;
 };
 
 const getRandomColor = () => {
-  const index = Math.floor(Math.random() * colors.length);
-  return colors[index];
+  return colors[Math.floor(Math.random() * colors.length)];
 };
 
 for (let i = 0; i < SQUARES_NUMBER; i++) {
   const square = document.createElement("div");
   square.classList.add("square");
 
-  square.addEventListener("mouseover", () => {
-    setColor(square);
-  });
+  square.addEventListener("mouseover", setColor);
 
-  square.addEventListener("mouseleave", () => {
-    removeColor(square);
-  });
+  square.addEventListener("mouseleave", removeColor);
 
   board.append(square);
 }
